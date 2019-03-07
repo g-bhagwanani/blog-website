869df1f002d5c753316b880a19c9331da07d09af
Gaurav Bhagwanani <gaurav.bhagwanani@gmail.com>
2019-03-07 20:31:32 +0530

    Added option to edit a post

--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,21 @@ def post_new(request):
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk = post.pk)
+    else:
+        form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form':form})
 
-    form = PostForm()
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
 
-    return render(request, 'blog/post_edit.html', {'form':form})
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+        return render(request, 'blog/post_edit.html', {'form': form})
